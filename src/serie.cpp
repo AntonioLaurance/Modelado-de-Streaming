8b28c8b15c29c6d7a8a471b2f1fcead665e7f81e
MoisesAdame <moisesadame01@gmail.com>
2022-06-11 09:26:20 -0500

    Modificaciones a la clase Serie la cual ahora hereda de Video para comportarse como uno y poder ser insertada en el vector videos de la clase catalogo

--- src/serie.cpp
+++ src/serie.cpp
@@ -1,14 +1,20 @@
 #include "serie.h"
 
-Serie::Serie()
+Serie::Serie() : Video()
 {
     seasons = 0;
 }
 
-Serie::Serie(vector<vector<Episode*> > episodes, int seasons)
+Serie::Serie(string ID, string name, string genre,  vector<vector<Episode*> > episodes) : Video()
 {
     this -> episodes = episodes;
-    this -> seasons = seasons;
+    seasons = episodes.size();
+    setID(ID);
+    setName(name);
+    setDuration(totalDuration());
+    setGenre(genre);
+    setRating(averageRating());
+    setRaters(totalRaters());
 }
 
 Serie::~Serie()
@@ -19,7 +25,7 @@ Serie::~Serie()
 string Serie::toString()
 {
     string txt = "";
-    txt += "[*] " + episodes[0][0]->getName() + "\n";
+    txt += "[*] " + name + "\n";
     vector<vector<Episode*> >::iterator itSeasons;
     vector<Episode*>::iterator itEpisodes;
     for(itSeasons = episodes.begin(); itSeasons != episodes.end(); itSeasons++){
@@ -31,6 +37,16 @@ string Serie::toString()
 } 
 
 float Serie::averageRating()
+{
+    return 0;
+}
+
+int Serie::totalDuration()
+{
+    return 0;
+}
+
+int Serie::totalRaters()
 {
     return 0;
 }
--- src/serie.h
+++ src/serie.h
@@ -5,9 +5,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "video.h"
 #include "episode.h"
 
-class Serie
+class Serie : public Video
 {
     private:
         vector<vector<Episode*> > episodes;
@@ -16,11 +17,13 @@ class Serie
     public:
         // Constructor and Destructor Method
         Serie();
-        Serie(vector<vector<Episode*> > episodes, int seasons);
+        Serie(string ID, string name, string genre,  vector<vector<Episode*> > episodes);
         ~Serie();
 
         string toString();
         float averageRating();
+        int totalDuration();
+        int totalRaters();
 
 };
 
